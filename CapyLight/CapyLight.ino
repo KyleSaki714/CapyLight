1b2a7bda4e59f5035988dc5b23ad7607322faf80
kyles13 <kyles13@cs.washington.edu>
2024-04-06 21:55:06 -0700

    mode switch state machine should be good

--- CapyLight/CapyLight.ino
+++ CapyLight/CapyLight.ino
@@ -6,69 +6,59 @@ typedef enum {
   NUMMODES
 } MODE;
 
-MODE _currentMode; 
 const int INPUT_BUTTON_MODESWITCH = 2;
+const int MODE_SWITCH_DELAY = 40; // in milliseconds
+
+MODE _currentMode;
 unsigned long _modeswitchbtn_lastontime;
-// int _modeswitch_lastbuttonstate;
-const int MODE_SWITCH_WINDOW = 40; // in milliseconds
+bool _modeswitchbtn_laststate;
 
 void setup() {
   Serial.begin(9600);
   _currentMode = CROSSFADE;
   _modeswitchbtn_lastontime = millis();
-  // _modeswitch_lastbuttonstate = 0;
+  _modeswitchbtn_laststate = LOW;
   pinMode(INPUT_BUTTON_MODESWITCH, INPUT_PULLUP);
 }
 
 void loop() {
-  
+
   checkChangeMode();
 
   switch (_currentMode) {
     case CROSSFADE:
-      // Serial.println("CROSSFADE MODE");
+      Serial.println("CROSSFADE MODE");
       break;
     case LOFI:
-      // Serial.println("LOFI MODE");
+      Serial.println("LOFI MODE");
       break;
     case HATS:
-      // Serial.println("HATS MODE");
+      Serial.println("HATS MODE");
       break;
   }
   delay(5);
 }
 
 void checkChangeMode() {
-  if (millis() - _modeswitchbtn_lastontime >= MODE_SWITCH_WINDOW) {
-    res = digitalRead(inputPin);
-    _modeswitchbtn_lastontime = millis();
-  }
-
   int buttonState = readButtonPullUp(INPUT_BUTTON_MODESWITCH);
   Serial.println(buttonState);
-  // Serial.print(" ");
-  // Serial.println(_modeswitch_lastbuttonstate);
-  if (buttonState == 1 && _modeswitch_lastbuttonstate == 0) {
+  if (buttonState != -1 && buttonState == HIGH) {
     _currentMode = (_currentMode + 1) % NUMMODES;
   }
-  _modeswitch_lastbuttonstate = buttonState;
 }
 
-// Using internal pull up resistor,
-// false is off, true is on.
-// Provides debouncing
-// int readButtonPullUp(int inputPin) {
-//   int res = 1;
-//   if (millis() - _modeswitchbtn_lastontime >= MODE_SWITCH_WINDOW) {
-//     res = digitalRead(inputPin);
-//     // Serial.println(buttonState);
-//     _modeswitchbtn_lastontime = millis();
-//     // Serial.println(_modeswitchbtn_lastontime);
-//   }
+/**
+ * Reads the chosen input pin assuming it is an INTERNAL_PULL_UP input pin.
+ * Reverses the values so that returning LOW means switch open, and HIGH means switch closed.
+ * Provides debouncing; returns -1 if called during a debounce window.
+*/
+int readButtonPullUp(int inputPin) {
+  int buttonPullUpVal = digitalRead(inputPin);
+  if (buttonPullUpVal != _modeswitchbtn_laststate && millis() - _modeswitchbtn_lastontime >= MODE_SWITCH_DELAY) {
+    _modeswitchbtn_laststate = buttonPullUpVal;
+    _modeswitchbtn_lastontime = millis();
 
-//   if (res == 1) {
-//     return 0;
-//   } else {
-//     return 1;
-//   }
-// }
+    return !_modeswitchbtn_laststate;
+  }
+  return -1;
+}
